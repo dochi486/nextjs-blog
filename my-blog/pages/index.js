1349a405537f0568ef6d4db01499ec8f4970d7cf
dochi486 <93mskim@gmail.com>
2022-10-12 19:03:55 +0900

    antd 설치, 홈 화면 레이아웃 구성 중

--- my-blog/pages/index.js
+++ my-blog/pages/index.js
@@ -1,22 +1,42 @@
 import styles from '../styles/Home.module.css';
 import SanityService from '../services/SanityService';
+import { Col, Row } from 'antd';
+import Link from 'next/link';
+import { CodeOutlined } from '@ant-design/icons';
 
 export default function Home({ home, posts }) {
   console.log(posts);
+
+  const mainPost = posts.find((p) => p.slug === home.mainPostUrl);
+  const otherPost = posts.filter((p) => p.slug !== home.mainPostUrl);
+
   return (
     <div className={styles.container}>
-      <h1>Sanity Blog Home</h1>
+      <Row
+        align="middle"
+        style={{
+          height: 64,
+        }}
+      >
+        <Col span={24}>
+          <Link href="/"></Link>
+          <a>
+            <div
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+              }}
+            >
+              <CodeOutlined /> My Blog
+            </div>
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const client = SanityClient({
-    dataset: 'production',
-    projectId: 'ozc58i66',
-    useCdn: process.env.NODE_ENV === 'production',
-  });
-
   const sanityService = new SanityService();
   const home = await sanityService.getHome();
   const posts = await sanityService.getPosts();
